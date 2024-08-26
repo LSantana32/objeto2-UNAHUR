607f65fb18b806a3c0bafacee0322889d27a95a8
LSantana32 <lucasnahuel.santana@estudiantes.unahur.edu.ar>
2024-08-26 16:34:39 -0300

    ejercicio terminado

--- SalaDeCine/src/ar/edu/unahur/po2/sala/SalaDeCine.java
+++ SalaDeCine/src/ar/edu/unahur/po2/sala/SalaDeCine.java
@@ -20,15 +20,61 @@ public class SalaDeCine {
 		}
 	}
 
-	public Boolean estaVacia() {		
+	public Boolean estaVacia() {
+		Boolean output=true;
 		for (int fila=0;fila<cantidadDeFilas;fila++) {
 			for (int columna=0;columna<butacasPorFilas;columna++) {
-				if (butacas[fila][columna] == true) {
-					return false;
+				if (butacas[fila][columna]==true){
+					output=false;
+					break;
+				}
+			}				
+		}
+		return output;
+		
+	}
+	
+	public Boolean estaOcupadaLaButaca(Integer fila,Integer columna) {
+		return butacas[fila][columna];
+	}
+	
+	public void ocuparButaca(Integer fila, Integer columna) {
+		if (butacas[fila][columna]==false) {
+			this.butacas[fila][columna]= true;
+		}
+	 }
+	
+	public Integer cantidadDeButacasOcupadas() {
+		Integer contador=0;
+		for (int fila=0;fila<cantidadDeFilas;fila++){
+			for (int asiento=0;asiento<butacasPorFilas;asiento++){
+				if (butacas[fila][asiento]==true) {
+					contador++;
 				}
 			}
 		}
-		return true;
+		return contador;
+				
 	}
+	
+	public Boolean hayEspacioPara(Integer cantidadDePersonas) {
+		Boolean output=false;
+		if (cantidadDePersonas<=butacasPorFilas) {
+			for (int fila=0;fila<cantidadDeFilas;fila++) {
+				Integer contador=0;
+				for (int asiento=0;asiento<butacasPorFilas;asiento++) {
+					if (butacas[fila][asiento]==false){
+					contador++;
+					}
+				}
+				if (contador>=cantidadDePersonas) {
+					output=true;
+					break;
+				}
+			}
+		}
+		return output;
+	}
+	
 
 }
--- SalaDeCine/test/ar/edu/unahur/po2/sala/SalaDeCineTest.java
+++ SalaDeCine/test/ar/edu/unahur/po2/sala/SalaDeCineTest.java
@@ -8,15 +8,66 @@ public class SalaDeCineTest {
 
 	@Test
 	public void queAlCrearLaSalaTodasLasButacasEstanVacias() {
-		//Preparación
+		//Preparaciï¿½n
 		Integer butacasTotales = 10;
 		Integer cantidaDeFilas = 2;
 		SalaDeCine sala = new SalaDeCine(butacasTotales,cantidaDeFilas);
 		Boolean valorEsperado = true;
-		//Ejecución
+		//Ejecuciï¿½n
 		Boolean valorObtenido = sala.estaVacia();
-		//Contrastación
+		//Contrastaciï¿½n
 		assertEquals(valorEsperado,valorObtenido);
 	}	
-
+	
+	@Test
+	public void verificarSiUnAsientoEstaOcupado() {
+		Integer butacasTotales = 10;
+		Integer cantidaDeFilas = 2;
+		SalaDeCine sala2 = new SalaDeCine(butacasTotales,cantidaDeFilas);
+		Boolean valorEsperado = false;
+		Boolean valorObtenido = sala2.estaOcupadaLaButaca(1,3);
+		assertEquals(valorEsperado,valorObtenido);
+	}
+	
+	@Test
+	public void verificarSiSePuedeOcuparAsiento() {
+		Integer butacasTotales = 10;
+		Integer cantidaDeFilas = 2;
+		SalaDeCine sala2 = new SalaDeCine(butacasTotales,cantidaDeFilas);
+		sala2.ocuparButaca(1,3);
+		Boolean valorEsperado = true;
+		Boolean valorObtenido = sala2.estaOcupadaLaButaca(1,3);
+		assertEquals(valorEsperado,valorObtenido);
+	}
+	
+	
+	@Test
+	public void verificarAsientosOcupados() {
+		Integer butacasTotales = 10;
+		Integer cantidaDeFilas = 2;
+		SalaDeCine sala2 = new SalaDeCine(butacasTotales,cantidaDeFilas);
+		sala2.ocuparButaca(1,3);
+		Integer valorEsperado = 1;
+		Integer valorObtenido = sala2.cantidadDeButacasOcupadas();
+		assertEquals(valorEsperado,valorObtenido);
+	}
+	
+	@Test
+	public void verificarSiHayEspacioParaNPersonasDeFormaContigua() {
+		Integer butacasTotales = 10;
+		Integer cantidaDeFilas = 2;
+		SalaDeCine sala2 = new SalaDeCine(butacasTotales,cantidaDeFilas);
+		sala2.ocuparButaca(1,3);
+		sala2.ocuparButaca(0, 3);
+		Boolean valorEsperado = false;
+		Boolean valorObtenido = sala2.hayEspacioPara(5);
+		assertEquals(valorEsperado,valorObtenido);
+	}
+	
+	
+	
+	
 }
+
+	
+
